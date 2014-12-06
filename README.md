e1fc1f44f41bbc65fb0ac42e31a2f3068377bfaa
tobikster <tobikster@gmail.com>
2014-12-06 22:20:44 +0100

    Hops count calculation added, reformat code to satisfy PEP8

--- README.md
+++ README.md
@@ -7,26 +7,31 @@ Program NetGeo jest bardzo intuicyjny w użyciu.
 Jedynym wymaganiem jest przygotowanie pliku wejściowego, w postaci adresów IP jakie mają zostać przebadane.
 Każdy adres musi być w osobnym wierszu.
 
-Następnie, program uruchamiany jest poprzez wywołanie komendy 
+Następnie, program uruchamiany jest poprzez wywołanie komendy
+
 ```
 NetGeo <ścieżka do pliku wejściowego>
 ```
 
 Rezultat działania programu domyślnie zapisywany jest w pliku result.csv, w lokalizacji, z jakiej został uruchomiony program.
 Lokalizację oraz nazwę pliku wyjściowego można zmienić korzystająć z opcji --output:
+
 ```
 NetGeo --output <nazwa pliku wyjściowego> <nazwa pliku wejściowego>
 ```
+
 Plik wynikowy zapisywany jest w formaci CSV, poszczególne pola oddzielane są średnikami.
 W kolumnach znajdują się informacje o adresie IP badanego węzła, jego fizycznej lokalizacji (podanej w radianach), odległości od lokalizacji komputera wyznaczonej w czasie uruchomienia programu, średniego czasu RTT oraz czasu pobierania domyślnej strony (pobieranej po podaniu adresu IP).
 Czas pobieranie jest znormalizowany, poprzez podzielenie faktycznego czasu pobierania przez jego rozmiar.
 
 Możliwa jest również zmiana domyślnego czasu oczekiwania na odpowiedzi podczas badania ping za pomocą parametru --pingTime
+
 ```
 NetGeo --pingTime <czas oczekiwania w sekundach> <nazwa pliku wejściowego>
 ```
 
 Opis dodatkowych opcji programu dostępny jest po wykonaniu polecenia.
+
 ```
 NetGeo --help
 ```
@@ -39,14 +44,18 @@ NetGeo dostępny jest jedynie na platformę Linux, jednak ewentualne portowanie 
 Szczegóły opisane są w podręczniku programisty.
 
 Aby zainstalować narzędzie, wystarczy pobrać kod źródłowy, np. za pomocą polecenia 
+
 ```
 git clone https://github.com/tobikster/NetGeo.git
 ```
+
 Następnie należy wyprodukować plik wykonywalny za pomocą skryptu *compile*, który znajduje się w głównym katalogu projektu.
+
 ```
 cd <ścieżka do repozytorium>
 ./compile
 ```
+
 Program zostanie zainstalowany w katalogu *bin* znajdującym się w folderze domowym bieżącego użytkownika.
 Dzięki temu będzie możliwe używanie go prosto z lini komend (bash)
 
--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 """
-Usage: NetGeo.py [options] INPUT_FILE 
+Usage: NetGeo.py [options] INPUT_FILE
 
 Options:
     -o --output OUTPUT      output file [default: result.csv]
@@ -7,116 +7,138 @@ Options:
     -v --verbose            Print progress
     -h --help               shows this message and exit
     --version               shows program version
-	
 """
 
 from __future__ import print_function
-from docopt import docopt
 from subprocess import Popen, PIPE
-import csv, json, math
+import csv
+import json
+import math
 import re
-import sys, os, platform
+import sys
+import platform
 import timeit
 import urllib2
+
+from docopt import docopt
+
 
 EARTH_RADIUS = 6371.009
 MAX_IP_LIST_SIZE = 1000
 VERSION = '1.0'
 
-def getHostLocation(ipAddr = ''):
-	location = None
-	try:
-		resp = urllib2.urlopen('http://ipinfo.io{}/json'.format('' if ipAddr == '' else '/{}'.format(ipAddr)))
-		if resp.getcode() == 200:
-			jsonResult = json.loads(resp.read())
-			if 'loc' in jsonResult and jsonResult['loc'] != '':
-				location = tuple([math.radians(float(x)) for x in jsonResult['loc'].split(',')])
-			else:
-				print('Result does not contains localization info for ip address "{}"!'.format(ipAddr), file=sys.stderr)
-		else:
-			print('WRN: Cannot estimate {0} location!'.format(ipAddr), file=sys.stderr)
-	except urllib2.HTTPError:
-		pass
-	return location
 
-def getDistance(start, end):
-	distance = None
-	if start != None and end != None:
-		deltaLatitude = start[0] - end[0]
-		meanLatitude = (start[0] + end[0]) / 2
-		deltaLongitude = start[1] - end[1]
-		distance = EARTH_RADIUS * math.sqrt(deltaLatitude**2 + (math.cos(meanLatitude) * deltaLongitude)**2)
-	else:
-		print("WRN: Either home host or target host localization are not known! Cannot calculate distance!", file=sys.stderr)
-	return distance
+def get_host_location(ip_address=''):
+    location = None
+    try:
+        resp = urllib2.urlopen('http://ipinfo.io{}/json'.format('' if ip_address == '' else '/{}'.format(ip_address)))
+        if resp.getcode() == 200:
+            json_result = json.loads(resp.read())
+            if 'loc' in json_result and json_result['loc'] != '':
+                location = tuple([math.radians(float(x)) for x in json_result['loc'].split(',')])
+            else:
+                print('Result does not contains localization info for ip address "{}"!'.format(ip_address), file=sys.stderr)
+        else:
+            print('WRN: Cannot estimate {0} location!'.format(ip_address), file=sys.stderr)
+    except urllib2.HTTPError:
+        pass
+    return location
 
-def getMeanRTT(ipAddr, deadline=5):
-	meanRTT = None
-	systemType= platform.system()
-	pingCommand = None
-	if systemType == 'Linux':
-		pingCommand = 'ping -qw {1} {0}'.format(ipAddr, deadline)
-	if pingCommand != None:
-		pingResult = Popen(pingCommand, shell=True, stdout=PIPE).stdout.read().splitlines()
-		for line in pingResult:
-			match = re.match('rtt min/avg/max/mdev = (?P<rttMin>.*)/(?P<rttAvg>.*)/(?P<rttMax>.*)/(?P<rttMdev>.*) ms', line)
-			if match != None:
-				meanRTT = match.group('rttAvg')
-		if meanRTT == None:
-			print('WRN: Host {0} does not answer for PING!'.format(ipAddr), file=sys.stderr)
-	return meanRTT
 
-def getDownloadTime(ipAddr):
-	result = None
-	try:
-		startTime = timeit.default_timer()
-		resp = urllib2.urlopen('http://{}'.format(ipAddr))
-		endTime = timeit.default_timer()
-		if resp.getcode() == 200:
-			result = (endTime - startTime) / len(resp.read())
-		else:
-			print('WRN: Cannot download default page from {0}'.format(ipAddr), file=std.stderr)
-	except (urllib2.HTTPError, urllib2.URLError):
-		print('WRN: Error while downloading default page from {0}'.format(ipAddr), file=sys.stderr)
-	return result
+def get_distance(start, end):
+    distance = None
+    if start is not None and end is not None:
+        delta_latitude = start[0] - end[0]
+        mean_latitude = (start[0] + end[0]) / 2
+        delta_longitude = start[1] - end[1]
+        distance = EARTH_RADIUS * math.sqrt(delta_latitude**2 + (math.cos(mean_latitude) * delta_longitude)**2)
+    else:
+        print("WRN: Either home host or target host localization are not known! Cannot calculate distance!", file=sys.stderr)
+    return distance
+
+
+def get_mean_rtt(ip_address, deadline=5):
+    mean_rtt = None
+    system_type = platform.system()
+    ping_command = None
+    if system_type == 'Linux':
+        ping_command = 'ping -qw {1} {0}'.format(ip_address, deadline)
+    if ping_command is not None:
+        ping_result = Popen(ping_command, shell=True, stdout=PIPE).stdout.read().splitlines()
+        for line in ping_result:
+            match = re.match('rtt min/avg/max/mdev = (?P<rttMin>.*)/(?P<rttAvg>.*)/(?P<rttMax>.*)/(?P<rttMdev>.*) ms', line)
+            if match is not None:
+                mean_rtt = match.group('rttAvg')
+        if mean_rtt is None:
+            print('WRN: Host {0} does not answer for PING!'.format(ip_address), file=sys.stderr)
+    return mean_rtt
+
+
+def get_hops_count(ip_address):
+    result = None
+    traceroute_command = None
+    system_type = platform.system()
+    if system_type == 'Linux':
+        traceroute_command = 'traceroute {0}'.format(ip_address)
+    if traceroute_command is not None:
+        traceroute_result = Popen(traceroute_command, shell=True, stdout=PIPE).stdout.read().splitlines()
+        result = len(traceroute_result[2:])
+    return result
+
+
+def get_download_time(ip_address):
+    result = None
+    try:
+        start_time = timeit.default_timer()
+        resp = urllib2.urlopen('http://{}'.format(ip_address))
+        end_time = timeit.default_timer()
+        if resp.getcode() == 200:
+            result = (end_time - start_time) / len(resp.read())
+        else:
+            print('WRN: Cannot download default page from {0}'.format(ip_address), file=sys.stderr)
+    except (urllib2.HTTPError, urllib2.URLError):
+        print('WRN: Error while downloading default page from {0}'.format(ip_address), file=sys.stderr)
+    return result
+
 
 def main():
-	args = docopt(__doc__, version=VERSION)
-	inputFilePath = args['INPUT_FILE']
-	outputFilePath = args['--output']
-	pingTime = args['--pingTime']
-	verbose = args['--verbose']
+    args = docopt(__doc__, version=VERSION)
+    input_file_path = args['INPUT_FILE']
+    output_file_path = args['--output']
+    ping_time = args['--pingTime']
+    verbose = args['--verbose']
 
-	if verbose:
-		print('Geting home host localization')
-	myLocalization = getHostLocation()
-	if myLocalization != None:
-		try:
-			with open(outputFilePath, 'w') as outputFile:
-				csvWriter = csv.writer(outputFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-				csvWriter.writerow(('IP address', 'localization', 'distance', 'RTT time', 'downlading time'))
-				with open(inputFilePath, 'r') as inputFile:
-					usedIpAddresses = []
-					for ipAddress in [x.strip() for x in inputFile]:
-						if len(usedIpAddresses) < MAX_IP_LIST_SIZE:
-							if ipAddress not in usedIpAddresses:
-								usedIpAddresses.append(ipAddress)
-								if verbose:
-									print('Processing {0}'.format(ipAddress))
-								targetLocalization = getHostLocation(ipAddress)
-								distance = getDistance(myLocalization, targetLocalization)
-								meanRTT = getMeanRTT(ipAddress, pingTime)
-								downloadTime = getDownloadTime(ipAddress)
-								csvWriter.writerow((ipAddress, targetLocalization, distance, meanRTT, downloadTime))
-							else:
-								print('WRN: IP address {0} has been checked before! Skipping'.format(ipAddress), file=sys.stderr)
-						else:
-							print('WRN: Maximum number of tested IP addresses ({0}) was reached! Breaking'.format(MAX_IP_LIST_SIZE))
-							break
-		except IOError:
-			print('ERR: Input/output error!')
-	else:
-		print('ERR: Cannot estimate home host localization! Exiting', file=sys.stderr)
+    if verbose:
+        print('Getting home host localization')
+    my_localization = get_host_location()
+    if my_localization is not None:
+        try:
+            with open(output_file_path, 'w') as outputFile:
+                csv_writer = csv.writer(outputFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(('IP address', 'localization', 'distance', 'RTT time', 'hops count', 'downloading time'))
+                with open(input_file_path, 'r') as inputFile:
+                    used_ip_addresses = []
+                    for ip_address in [x.strip() for x in inputFile]:
+                        if len(used_ip_addresses) < MAX_IP_LIST_SIZE:
+                            if ip_address not in used_ip_addresses:
+                                used_ip_addresses.append(ip_address)
+                                if verbose:
+                                    print('Processing {0}'.format(ip_address))
+                                target_localization = get_host_location(ip_address)
+                                distance = get_distance(my_localization, target_localization)
+                                mean_rtt = get_mean_rtt(ip_address, ping_time)
+                                hops_count = get_hops_count(ip_address)
+                                download_time = get_download_time(ip_address)
+                                csv_writer.writerow((ip_address, target_localization, distance, mean_rtt, hops_count, download_time))
+                            else:
+                                print('WRN: IP address {0} has been checked before! Skipping'.format(ip_address), file=sys.stderr)
+                        else:
+                            print('WRN: Maximum number of tested IP addresses ({0}) was reached! Breaking'.format(MAX_IP_LIST_SIZE))
+                            break
+        except IOError:
+            print('ERR: Input/output error!')
+    else:
+        print('ERR: Cannot estimate home host localization! Exiting', file=sys.stderr)
 
 if __name__ == '__main__':
-	main()
+    main()
